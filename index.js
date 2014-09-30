557d2734a1d93641d2ca2b207c4b1d81b85732ce
Fabien O'Carroll <fabien@allou.is>
2014-09-30 12:50:54 +0100

    Begin adding crude support for skipping closed issues

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+
 var nextButtonHTML = '<button type="button" class="minibutton js-next-issue">Next</button>';
 var buttonsContainer = document.querySelector('.gh-header-actions');
  
-var currentURL = window.location.href;
+var currentURL = window.location.pathname;
 var parts = currentURL.split('/');
 parts[parts.length - 1]--; // next issue - but going back because usually start at the end
-var nextURL = parts.join('/');
+var nextURL = window.location.origin + parts.join('/') + '?fromNextButton';
+
+function goToNextIssue () {
+  window.location.href = nextURL;
+}
  
+if (window.location.search.indexOf('fromNextButton') !== -1) {
+  if (document.querySelectorAll('.state.state-open').length === 0) {
+    goToNextIssue();
+  }
+}
+
 buttonsContainer.innerHTML += nextButtonHTML;
  
 var nextButton = buttonsContainer.querySelector('.js-next-issue');
-nextButton.addEventListener('click', function () {
-  window.location.href = nextURL;
-});
+nextButton.addEventListener('click', goToNextIssue);
